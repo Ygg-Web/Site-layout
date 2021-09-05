23a5c6beb29da009ecb55fbf466d902009c0b4d9
Nikita <TsarevNF@yandex.ru>
2021-09-05 20:38:36 +0300

    creating  modal windows using javascript

--- first-layout/js/script.js
+++ first-layout/js/script.js
@@ -1,2 +1,40 @@
 const btnEntry = document.querySelector('.btn-entrance'),
     btnRegistration = document.querySelector('.btn-registration'),
+    modal = document.querySelector('.modal'),
+    modalRegistration = document.querySelector('.modal_registration');
+// modalRegistration = document.querySelector('[data-registration]');
+
+function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+}
+
+function openModalRegistration() {
+    modalRegistration.classList.add('show');
+    modalRegistration.classList.remove('hide');
+}
+
+btnEntry.addEventListener('click', openModal);
+btnRegistration.addEventListener('click', openModalRegistration);
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+}
+
+function closeModalRegistration() {
+    modalRegistration.classList.add('hide');
+    modalRegistration.classList.remove('show');
+}
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal || event.target.getAttribute('data-close') == '') {
+        closeModal();
+    }
+});
+
+modalRegistration.addEventListener('click', (event) => {
+    if (event.target === modalRegistration || event.target.getAttribute('data-close') == '') {
+        closeModalRegistration();
+    }
+});
