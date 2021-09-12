69b61878dd926022bde64d150d405aa7d363c1b1
Nikita <TsarevNF@yandex.ru>
2021-09-12 20:01:31 +0300

    added adaptive in css in all sections

--- first-layout/js/script.js
+++ first-layout/js/script.js
@@ -26,3 +26,79 @@ function closeWindow(nameModal) {
 
 closeWindow(modal);
 closeWindow(modalRegistration);
+
+
+// ==Slider=================================================================
+const
+    sliders = document.querySelectorAll('.tariff__item'),
+    sliderLine = document.querySelector('.tariffs__line'),
+    dots = document.querySelectorAll('.dot');
+
+let index = 0;
+let width;
+
+const init = () => {
+    width = document.querySelector('.tariff__colum').offsetWidth;
+    sliderLine.style.width = width * sliders.length + 'px';
+    sliders.forEach(slider => {
+        slider.style.width = width + 'px';
+        slider.style.height = 'auto';
+    });
+    // rollSlider();
+}
+
+window.addEventListener('resize', init);
+init();
+
+let rollSlider = () => {
+    sliderLine.style.transform = 'translate(-' + index * width + 'px)';
+}
+
+const activeSlide = n => {
+    sliders.forEach(slide => {
+        slide.classList.remove('active');
+    })
+    sliders[n].classList.add('active');
+}
+
+const activeDot = n => {
+    dots.forEach(dot => {
+        dot.classList.remove('active');
+    })
+    dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => {
+    activeSlide(ind);
+    activeDot(ind);
+    rollSlider();
+}
+
+const nextSlide = () => {
+    if (index == sliders.length - 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    prepareCurrentSlide(index);
+}
+
+const prevSlide = () => {
+    if (index == 0) {
+        index = sliders.length - 1;
+    } else {
+        index--;
+    }
+    prepareCurrentSlide(index);
+}
+
+// next.addEventListener('click', nextSlide);
+// prev.addEventListener('click', prevSlide);
+
+dots.forEach((dot, indexDot) => {
+    dot.addEventListener('click', () => {
+        index = indexDot;
+        rollSlider();
+        prepareCurrentSlide(index);
+    });
+})
