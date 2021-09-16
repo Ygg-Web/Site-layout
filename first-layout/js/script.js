293745d8a89926e81a3cd9d457ad1cb47ac028d1
Nikita <TsarevNF@yandex.ru>
2021-09-16 22:40:36 +0300

    creating text sliders using js

--- first-layout/js/script.js
+++ first-layout/js/script.js
@@ -32,160 +32,81 @@ closeWindow(modalRegistration);
 const
     slidersTariff = document.querySelectorAll('.tariff__item'),
     sliderLineTariff = document.querySelector('.tariffs__line'),
+    sliderBlockWidthTariff = document.querySelector('.tariff__slider'),
+    sliderColumWidthTariff = document.querySelector('.tariff__colum'),
+    dotsTraffic = document.querySelectorAll('.dot-tariff'),
     slidersComment = document.querySelectorAll('.comment__item'),
     sliderLineComment = document.querySelector('.comments__line'),
-    dotsComment = document.querySelectorAll('.dot-comment');
-dotsTraffic = document.querySelectorAll('.dot-tariff');
+    sliderBlockWidthComment = document.querySelector('.comment__slider'),
+    sliderColumWidthComment = document.querySelector('.comment__colum'),
+    dotsComment = document.querySelectorAll('.dot-comment'),
+    slidersStep = document.querySelectorAll('.step__item'),
+    sliderLineStep = document.querySelector('.steps__line'),
+    sliderBlockWidthStep = document.querySelector('.step__slider'),
+    sliderColumWidthStep = document.querySelector('.step__colum'),
+    dotsStep = document.querySelectorAll('.dot-step');
 
 let index = 0;
 let width;
 
-// const init = (blockWidth, blockLine, blockSliders) => {
-//     // width = document.querySelector(`${blockWidth}`).offsetWidth;
-//     // console.log(width);
-//     width = blockWidth;
-//     // blockLine.style.width = width * blockSliders.length + 'px';
-//     blockLine.style.width = width * 3 + 'px';
-//     console.log(blockLine.style.width);
-//     blockSliders.forEach(slider => {
-//         slider.style.width = width + 'px';
-//         slider.style.height = 'auto';
-//     });
-//     // rollSlider();
-// }
-
-// window.addEventListener('resize', init);
-// init(400, sliderLineTariff, slidersTariff);
-// // init('.comments', sliderLineComment, slidersComment);
-
-// const rollSlider = (blockLine) => {
-//     blockLine.style.transform = 'translate(-' + index * width + 'px)';
-// }
-
-// const activeSlide = (n, blockSliders) => {
-//     blockSliders.forEach(slide => {
-//         slide.classList.remove('active');
-//     })
-//     blockSliders[n].classList.add('active');
-// }
-
-// const activeDot = (n, blockDots) => {
-//     blockDots.forEach(dot => {
-//         dot.classList.remove('active');
-//     })
-//     blockDots[n].classList.add('active');
-// }
-
-// const prepareCurrentSlide = (ind, blockSliders, blockLine, blockDots) => {
-//     activeSlide(ind, blockSliders);
-//     activeDot(ind, blockDots);
-//     rollSlider(blockLine);
-// }
-
-// dotsTraffic.forEach((dot, indexDot) => {
-//     dot.addEventListener('click', () => {
-//         index = indexDot;
-//         prepareCurrentSlide(index, slidersTariff, sliderLineTariff, dotsTraffic);
-//     });
-// })
-
-
-
-
-
-// +====================================================
-
-// const init = () => {
-//     width = document.querySelector('.tariff__slider').offsetWidth;
-//     sliderLineTariff.style.width = width * slidersTariff.length + 'px';
-//     console.log(sliderLineTariff.style.width);
-//     slidersTariff.forEach(slider => {
-//         slider.style.width = document.querySelector('.tariff__colum').offsetWidth + 'px';
-//         slider.style.height = 'auto';
-//     })
-// }
-
-// window.addEventListener('resize', init);
-// init();
-// const rollSlider = () => {
-//     sliderLineTariff.style.transform = 'translate(-' + index * width + 'px)';
-// }
-
-
-// const activeSlide = n => {
-//     slidersTariff.forEach(slide => {
-//         slide.classList.remove('active');
-//     })
-//     slidersTariff[n].classList.add('active');
-// }
-
-// const activeDot = n => {
-//     dotsTraffic.forEach(dot => {
-//         dot.classList.remove('active');
-//     })
-//     dotsTraffic[n].classList.add('active');
-// }
-
-// const prepareCurrentSlide = ind => {
-//     activeSlide(ind);
-//     activeDot(ind);
-// }
-
-// dotsTraffic.forEach((dot, indexDot) => {
-//     dot.addEventListener('click', () => {
-//         index = indexDot;
-//         rollSlider();
-//         prepareCurrentSlide(index);
-//     });
-// })
-
-
-
-// ================================================
-
-const init = () => {
-    width = document.querySelector('.comment__slider').offsetWidth;
-    sliderLineComment.style.width = width * slidersComment.length + 'px';
-    slidersComment.forEach(slider => {
-        slider.style.width = document.querySelector('.comment__colum').offsetWidth + 'px';
+const init = (blockWidthSlider, blockLine, blockSliders, blockColumWidth) => {
+    width = blockWidthSlider.offsetWidth;
+    blockLine.style.width = width * blockSliders.length + 'px';
+    blockSliders.forEach(slider => {
+        slider.style.width = blockColumWidth.offsetWidth + 'px';
         slider.style.height = 'auto';
-    })
+    });
+    // rollSlider();
 }
 
+// window.addEventListener('resize', init);
+init(sliderBlockWidthTariff, sliderLineTariff, slidersTariff, sliderColumWidthTariff);
+init(sliderBlockWidthComment, sliderLineComment, slidersComment, sliderColumWidthComment);
+init(sliderBlockWidthStep, sliderLineStep, slidersStep, sliderColumWidthStep);
 
-window.addEventListener('resize', init);
-init();
-const rollSlider = () => {
-    sliderLineComment.style.transform = 'translate(-' + index * width + 'px)';
+const rollSlider = (blockLine) => {
+    blockLine.style.transform = 'translate(-' + index * width + 'px)';
 }
 
-
-const activeSlide = n => {
-    slidersComment.forEach(slide => {
+const activeSlide = (n, blockSliders) => {
+    blockSliders.forEach(slide => {
         slide.classList.remove('active');
     })
-    slidersComment[n].classList.add('active');
+    blockSliders[n].classList.add('active');
 }
 
-const activeDot = n => {
-    dotsComment.forEach(dot => {
+const activeDot = (n, blockDots) => {
+    blockDots.forEach(dot => {
         dot.classList.remove('active');
     })
-    dotsComment[n].classList.add('active');
+    blockDots[n].classList.add('active');
 }
 
-const prepareCurrentSlide = ind => {
-    activeSlide(ind);
-    activeDot(ind);
+const prepareCurrentSlide = (ind, blockSliders, blockLine, blockDots) => {
+    activeSlide(ind, blockSliders);
+    activeDot(ind, blockDots);
+    rollSlider(blockLine);
 }
+
+dotsTraffic.forEach((dot, indexDot) => {
+    dot.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index, slidersTariff, sliderLineTariff, dotsTraffic);
+    });
+})
 
 dotsComment.forEach((dot, indexDot) => {
     dot.addEventListener('click', () => {
         index = indexDot;
-        rollSlider();
-        prepareCurrentSlide(index);
+        prepareCurrentSlide(index, slidersComment, sliderLineComment, dotsComment);
     });
 })
 
-// ===возможно переверстать блоки с комментами
-// ===сделать слайдер под steps чтобы тестить функции из js
+dotsStep.forEach((dot, indexDot) => {
+    dot.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index, slidersStep, sliderLineStep, dotsStep);
+    });
+})
+
+// =============================================================================
